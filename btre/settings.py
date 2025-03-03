f0d5613097137503f9f8a5f5c0fdb14e1212e7ac
Dominic <dommos26@bergen.org>
2025-03-03 10:02:25 -0500

    update

--- btre/settings.py
+++ btre/settings.py
@@ -33,7 +33,7 @@ SECRET_KEY = SECRET_KEY
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['https://btrealestate-production-26f1.up.railway.app/']
+ALLOWED_HOSTS = ['btrealestate-production-26f1.up.railway.app/']
 
 # Application definition
 
